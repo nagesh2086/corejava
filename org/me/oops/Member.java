01cf1bdc6b5ebbb7cd9749ac116565aa08834f5f
Kekan Nagesh <nagesh.kekan@tieto.com>
2018-12-14 11:13:58 +0530

    2. Association - Distributing and sharing responsibilities, Community --> Member

---
+++ org/me/oops/Member.java
@@ -0,0 +1,15 @@
+package org.me.oops;
+
+/**
+ * Association - Distributing and sharing responsibilities.
+ * 
+ * Community has dependency on Member
+ * 
+ * Community --> Member
+ * 
+ * @author kekannag
+ *
+ */
+public class Member {
+
+}
