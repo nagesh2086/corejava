10c34bdfcc96dac09773c98db96310ac51f969d9
nagesh2086 <nagesh2086@gmail.com>
2019-02-04 11:06:32 +0530

    Java is Pass by Value and Not Pass by Reference.

--- org/me/core/java/JavaIsPassByValueAlways.java
+++ org/me/core/java/JavaIsPassByValueAlways.java
@@ -3,9 +3,16 @@ package org.me.core.java;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Java is Pass by Value and Not Pass by Reference.
+ * 
+ * @author kekannag
+ *
+ */
 public class JavaIsPassByValueAlways {
 	public static void main(String[] args) {
 		// test: passing primitive as method arguments
+
 		int x = 1;
 		int y = 2;
 		System.out.print("Values of x & y before primitive modification: ");
@@ -15,6 +22,7 @@ public class JavaIsPassByValueAlways {
 		System.out.println(" x = " + x + " ; y = " + y + "\n");
 
 		// test: passing wrapper as method arguments
+
 		Integer obj1 = new Integer(1);
 		Integer obj2 = new Integer(2);
 		System.out.print("Values of obj1 & obj2 before wrapper modification: ");
@@ -49,9 +57,23 @@ public class JavaIsPassByValueAlways {
 
 	private static void modifyList(List<Integer> lstParam) {
 		lstParam.add(2);
+		lstParam = new ArrayList<>();
+		lstParam.add(1);
+		lstParam.add(2);
+		lstParam.add(3);// now size becomes 3 and lstParam is referring different object.
 	}
 
+	/**
+	 * We are setting name by using given student reference of Student class then
+	 * making student reference to refer another object.
+	 * 
+	 * @param student - it is only reference value which is same as when called this
+	 *                method.
+	 */
 	private static void modifyStudent(Student student) {
 		student.setName("Alex");
+		student = new Student();
+		student.setName("Janardhan");
+
 	}
 }
