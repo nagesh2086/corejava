f6a75cacf2695c6ea0adba449aefb74ee9f356bb
nagesh2086 <nagesh2086@gmail.com>
2021-03-24 13:51:45 +0530

    modified SolverTest which is example of cyclic barrier

--- SolverTest.java
+++ SolverTest.java
@@ -5,6 +5,7 @@ public class SolverTest {
 		float[] matrix = new float[] { 0, 2, 3, 4, 5, 6, 7, 8, 9, 5, 4, 1, 25, 6, 3, 8, 1, 2, 1, 5, 7, 4, 1, 2, 5, 0, 1,
 				5, 2, 5, 5, 2.3f };
 		Solver s = new Solver(matrix);
+		Class<SolverTest> intrinsicLock = SolverTest.class;
 	}
 
 }
