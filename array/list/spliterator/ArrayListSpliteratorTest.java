28d80b5635edbaadffde9feacdc3d7af6c868ee4
nagesh2086 <nagesh2086@gmail.com>
2021-03-27 17:32:46 +0530

    - initial size of spliterator is whole initial array
    - if we have consumed created spliterator and tries to consume again by
    using trySplit() then can see NPE because source spliterator has been
    consumed already.
    
    - without consuming there is no issue

--- array/list/spliterator/ArrayListSpliteratorTest.java
+++ array/list/spliterator/ArrayListSpliteratorTest.java
@@ -10,26 +10,35 @@ public class ArrayListSpliteratorTest {
 		List<Integer> list = new ArrayList<>();
 		int listLength = 1000;
 		for (int i = 0; i < listLength; i++) {
-			list.add(++i);
+			list.add(i);
 		}
 		System.out.println(list);
 
-		/*
-		 * boolean tryAdvance = list.spliterator().tryAdvance(e ->
-		 * System.out.println(e)); System.out.println(tryAdvance);
-		 * 
-		 * Spliterator<Integer> trySplit1 = list.spliterator().trySplit();
-		 * System.out.println(trySplit1.getExactSizeIfKnown());
-		 * trySplit1.forEachRemaining(e -> System.out.println(e));
-		 * 
-		 * Spliterator<Integer> trySplit2 = list.spliterator().trySplit();
-		 * System.out.println(trySplit1.getExactSizeIfKnown());
-		 * trySplit2.forEachRemaining(e -> System.out.println(e));
-		 */
+		/*boolean tryAdvance = list.spliterator().tryAdvance(e -> System.out.println(e));
+		System.out.println(tryAdvance);*/
+
+		/*Spliterator<Integer> trySplit1 = list.spliterator().trySplit();
+		System.out.println(trySplit1.getExactSizeIfKnown());
+		trySplit1.forEachRemaining(e -> System.out.println(e));
+
+		Spliterator<Integer> trySplit2 = list.spliterator().trySplit();
+		System.out.println(trySplit1.getExactSizeIfKnown());
+		trySplit2.forEachRemaining(e -> System.out.println(e));*/
 
 		Spliterator<Integer> trySplit3 = list.spliterator();
 		System.out.println(trySplit3.estimateSize());
-		trySplit3.forEachRemaining(e -> System.out.println(e));
+		//trySplit3.forEachRemaining(e -> System.out.print(e + ", "));
+		System.out.println();
+
+		/*Spliterator<Integer> trySplit4 = list.spliterator();
+		System.out.println(trySplit4.estimateSize());
+		trySplit4.forEachRemaining(e -> System.out.print(e + ", "));
+		System.out.println();*/
+		
+		Spliterator<Integer> trySplit5 = trySplit3.trySplit();
+		System.out.println(trySplit5.estimateSize());
+		trySplit5.forEachRemaining(e -> System.out.print(e + ", "));
+
 	}
 
 }
