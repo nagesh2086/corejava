b09be81359852abdf298659c24b9cb9c646c5fd4
nagesh2086 <nagesh2086@gmail.com>
2021-04-01 17:55:38 +0530

    try finally return value test - if we have return statement in try block in method then try-block value will be returned - if we have return statement at end then finally block value will be returned

--- ValueOfVarInFinally.java
+++ ValueOfVarInFinally.java
@@ -2,20 +2,28 @@
 public class ValueOfVarInFinally {
 
 	public static void main(String[] args) {
+		int tryFinallyTest = tryFinallyTest();
+		System.out.println("After return: " + tryFinallyTest);
+	}
+
+	public static int tryFinallyTest() {
 		int x = 10;
 		System.out.println("Original: " + x);
-		
+
 		try {
 			x = 100;
 			System.out.println("Try: " + x);
-			return;
+			//return x;
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			x = 200;
 			System.out.println("Finally: " + x);
 		}
-		System.out.println("End: " + x);
+		
+		System.out.println("Before return and END: " + x);
+		return x;
+
 	}
 
 }
